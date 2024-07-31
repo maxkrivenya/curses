7e087e5a9094223590dd994f0a7dc57eae70d751
maxkrivenya <maxkrivenya@gmail.com>
2024-07-31 21:56:52 +0300

    added opt parameters to frame::main to launch rain && hacker_screen

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include "fun_ctions.h"
 
 
-int main(){
+int main(int argc, char* argv[]){
+    if(argc > 1){
+        char c = getopt(argc, argv, "rh");
+        switch(c) {
+            case 'r':{ rain(); return 0;}
+            case 'h':{ hacker_screen(); return 0;}
+            default:{printf("%s%sERROR: UNKNOWN OPT\n%s", BACK_BLACK, FORE_RED, RESET); return 1;}
+        }
+    }
+
+
+
     struct List* framestack = new_list();
 
     struct Frame* console;
