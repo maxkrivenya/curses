9c67c35d363b1f335d56086aa36a31403e49659b
maxkrivenya <maxkrivenya@gmail.com>
2024-08-06 12:47:53 +0300

    how did i lose a header bro

---
+++ src/headers/framestack.h
@@ -0,0 +1,9 @@
+#ifndef __framestack_h_
+#define __framestack_h_
+
+#include "frame.h"
+void framestack_push_tail(struct List* framestack, struct Frame* fr);
+void framestack_print(struct List* framestack, struct Node* frame_ptr);
+void framestack_render(struct List* framestack, struct Node* frame_ptr);
+
+#endif
